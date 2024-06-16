4469eb3d3d09bc11b6a98056a693f364a66ebfd4
LucasMisk <lucasmis55@gmail.com>
2024-06-16 17:42:51 +0300

    Lucas PC 2

--- q.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-# Check if GPU acceleration is enabled
-print("GPU available:" , tf.config.list_physical_devices('GPU'))
-print("Is GPU available for TensorFlow:", tf.test.is_gpu_available())
